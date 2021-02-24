bd9ab7795a2c2175ef1d6d5dbfb95626d04cf91c
BRANISLAV BIELIK <bielik@centrum.sk>
2021-02-25 00:09:45 +0100

    BBI web xamarin added

---
+++ CleanXamarin/App/MyNewProject.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyNewProject.Web.Models;
+using Ooui.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyNewProject.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+          
+            var page = new MyNewProject.UI.Orders.Views.MainPage();
+            var element = page.GetOouiElement();
+            return new ElementResult(element, "Hello from XAML!");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CleanXamarin/App/MyNewProject.Web/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Xamarin.Forms;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MyNewProject.Web
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            //InitializeComponent();
+        }
+
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var button = sender as Button;
+
+            if (button == null)
+                return;
+
+            count++;
+            button.Text = $"You clicked {count} times!";
+        }
+    }
+}
